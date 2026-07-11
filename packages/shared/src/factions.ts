6241fc0e4b68dd052512c451afa8f912be5ebed5
pittonje <burnususandrey@gmail.com>
2026-07-11 10:20:47 +0300

    refactor: isolate profile message contract

--- packages/shared/src/factions.ts
+++ packages/shared/src/factions.ts
@@ -1,2 +1,2 @@
 export type Faction = 'red' | 'blue';
-export type JoinMode = 'player' | 'spectator';
+export type { JoinMode } from './profile-contract.js';
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,12 @@ export * from './constants.js';
 export * from './factions.js';
 export * from './messages.js';
 export * from './movement.js';
+export {
+  ProfileClientMessages,
+  ProfileServerMessages,
+  type JoinMode,
+  type JoinRequest,
+  type ProfileAcceptedMessage,
+  type ProfileRejectedMessage,
+  type RoomParticipant
+} from './profile-contract.js';
